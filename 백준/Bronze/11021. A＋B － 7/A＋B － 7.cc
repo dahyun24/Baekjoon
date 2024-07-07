deb554de2b2bc42676f0727a91343ba14677fd13
dana <123882512+dahyun24@users.noreply.github.com>
2024-07-07 23:53:38 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int num,a,b;
+    cin >> num;
+    for (int i=0;i<num ; i++){
+        cin >> a >> b;
+        cout << "Case #" << i+1 << ": " << a+b << "\n";
+    }
+
+    return 0;
+}
