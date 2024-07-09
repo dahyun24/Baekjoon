503d9733bc25e8d995799e32aef754bc04f42b01
dana <123882512+dahyun24@users.noreply.github.com>
2024-07-10 00:10:43 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    char A;
+    cin >> A;
+
+    cout << int(A);
+
+	return 0;
+}
