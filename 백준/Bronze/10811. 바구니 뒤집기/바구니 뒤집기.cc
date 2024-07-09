341bb5bfbc47351a4d96b0f3927cb035f6fbc463
dana <123882512+dahyun24@users.noreply.github.com>
2024-07-09 23:31:18 +0900

    Update 바구니 뒤집기.cc

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <algorithm>
+#include <algorithm> // 추가하기
 using namespace std;
 
 int main() {
@@ -10,14 +10,14 @@ int main() {
     int N, M;
     cin >> N >> M;
 
-    int* arr = new int[N+1];
+    int* arr = new int[N+1]; // 첫번째 arr[0]은 0으로 그냥 두고 1부터 N까지 사용
     for (int i = 1; i <= N; i++) {
         arr[i] = i;
     }
     int a, b;
     for (int i = 0; i < M; i++) {
         cin >> a >> b;
-        reverse(arr+a,arr+b+1);
+        reverse(arr+a,arr+b+1); // arr 배열의 a+1번째부터 b+1번째까지 뒤집어라 , arr[0] = 0 이므로 1번째 부터 시작
     }
 
     for (int i = 1; i <= N; i++)
