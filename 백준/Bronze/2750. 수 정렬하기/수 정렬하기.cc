6ca2ee229e94c4d00844453cff3746860c1260b1
우다현(DaHyun Woo) <123882512+dahyun24@users.noreply.github.com>
2024-08-12 23:43:10 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,25 +1,25 @@
 #include <iostream>
-#include <string>
-
+#include <queue>
+#include <vector>
 using namespace std;
-int main() {
+
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
+    // bubble sort
     int num;
     cin >> num;
-    int* arr = new int[num];
+    int *arr = new int[num];
     for (int i = 0; i < num; i++) {
         cin >> arr[i];
     }
-    for (int i = 0; i < num; i++) {
-        for (int j = 0; j < num-1; j++) {
+
+    for (int i = 0; i < num-1; i++) {
+        for (int j = 0; j < num-1-i; j++) {
             if (arr[j] > arr[j + 1]) {
-                int temp;
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                swap(arr[j], arr[j + 1]);
             }
         }
     }
@@ -27,6 +27,6 @@ int main() {
         cout << arr[i] << "\n";
     }
 
-    delete arr;
+    delete[] arr;
     return 0;
 }
