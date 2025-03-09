d86429ee5106f83b031067af99bf6be10c2701bd
다현 <123882512+dahyun24@users.noreply.github.com>
2025-03-09 21:05:38 +0900

    [Silver IV] Title: 스택 2, Time: 824 ms, Memory: 233564 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -4,11 +4,19 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
+// 2번째 제출
+/**
+ * System.out.println으로 출력하는 것보다 StringBuilder에 쌓아뒀다가 한번에 출력하는 것이 좋다
+ * Stack을 static으로 처음에 선언하기
+ * */
 public class Main {
-	public static void main(String[] args) throws IOException {
-		Stack<Integer> stack = new Stack<>();
+	static final Stack<Integer> stack = new Stack<>();
+	// 쌓아서 출력하는 방법
+	static final StringBuilder sb = new StringBuilder();
 
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		// 첫째 줄에 명령의 수 N
 		int n = Integer.parseInt(br.readLine());
 		for (int i = 0; i < n; i++) {
@@ -16,61 +24,62 @@ public class Main {
 			int a = Integer.parseInt(st.nextToken());
 			switch (a) {
 				case 1:
-					putStack(stack, Integer.parseInt(st.nextToken()));
+					putStack(Integer.parseInt(st.nextToken()));
 					break;
 				case 2:
-					printStack1(stack);
+					printStack1();
 					break;
 				case 3:
-					countStack(stack);
+					countStack();
 					break;
 				case 4:
-					isEmptyStack(stack);
+					isEmptyStack();
 					break;
 				case 5:	
-					printStack2(stack);
+					printStack2();
 					break;
 			}
 		}
+		System.out.println(sb);
 	}
 
 	// 1
-	private static void putStack(Stack<Integer> stack, int i) {
+	private static void putStack(int i) {
 		stack.push(i);
 	}
 
 	// 2
-	private static void printStack1(Stack<Integer> stack) {
+	private static void printStack1() {
 		if (stack.isEmpty()) {
-			System.out.println("-1");
+			sb.append(-1).append("\n");
 		}
 		else {
-			System.out.println(stack.pop());
+			sb.append(stack.pop()).append("\n");
 		}
 	}
 
 	// 3
-	private static void countStack(Stack<Integer> stack) {
-		System.out.println(stack.size());
+	private static void countStack() {
+		sb.append(stack.size()).append("\n");
 	}
 
 	// 4
-	private static void isEmptyStack(Stack<Integer> stack) {
+	private static void isEmptyStack() {
 		if (stack.isEmpty()) {
-			System.out.println("1");
+			sb.append(1).append("\n");
 		}
 		else {
-			System.out.println("0");
+			sb.append(0).append("\n");
 		}
 	}
 
 	// 5
-	private static void printStack2(Stack<Integer> stack) {
+	private static void printStack2() {
 		if (stack.isEmpty()) {
-			System.out.println("-1");
+			sb.append(-1).append("\n");
 		}
 		else {
-			System.out.println(stack.peek());
+			sb.append(stack.peek()).append("\n");
 		}
 	}
 }
