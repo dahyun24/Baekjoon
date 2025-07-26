3170c8a05e69d5dd59e151129e93273cd8f143f6
다현 <123882512+dahyun24@users.noreply.github.com>
2025-07-26 21:52:05 +0900

    [Silver V] Title: 피보나치 수 4, Time: 148 ms, Memory: 21240 KB -BaekjoonHub

--- 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -7,10 +7,9 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		BigInteger[] fib = new BigInteger[n+1];
 		BigInteger a = BigInteger.ZERO;
 		BigInteger b = BigInteger.ONE;
-
+        // 피보나치 수는 직전 2개 값만 알면 다음 값을 계산할 수 있기 때문에 배열이 없어도 된다.
 		for (int i = 2; i <= n; i++) {
 			BigInteger temp = a.add(b);
 			a = b;
