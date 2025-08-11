d3b8dd082fa874794dc67a8de77106abc78ee801
다현 <123882512+dahyun24@users.noreply.github.com>
2025-08-11 23:12:17 +0900

    [Bronze III] Title: 공, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+
+        int cup = 1;
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a == cup || b == cup) {
+                cup = cup == a ? b : a;
+            }
+        }
+        System.out.println(cup);
+    }
+}
