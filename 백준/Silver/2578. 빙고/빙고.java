83afad310618c0d9270c98f8a636ed45d0f3b8cc
다현 <123882512+dahyun24@users.noreply.github.com>
2025-08-13 21:49:54 +0900

    [Silver IV] Title: 빙고, Time: 172 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[][] bingo = new int[5][5];
+    static int count;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                bingo[i][j] = sc.nextInt();
+            }
+        }
+        for (int k = 1; k <= 25; k++) {
+            int call = sc.nextInt();
+
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (bingo[i][j] == call) {
+                        bingo[i][j] = 0;
+                    }
+                }
+            }
+
+            rCheck();
+            cCheck();
+            lrCheck();
+            rlCheck();
+
+            if (count >= 3) {
+                System.out.println(k);
+                break;
+            }
+            count = 0;
+        }
+    }
+
+    public static void rCheck() {
+        for(int i = 0; i < 5; i++) {
+            int zeroCount = 0;
+            for(int j = 0; j < 5; j++) {
+                if(bingo[i][j] == 0)
+                    zeroCount++;
+            }
+            if(zeroCount == 5)
+                count++;
+        }
+    }
+
+    public static void cCheck() {
+        for(int i = 0; i < 5; i++) {
+            int zeroCount = 0;
+            for(int j = 0; j < 5; j++) {
+                if(bingo[j][i] == 0)
+                    zeroCount++;
+            }
+            if(zeroCount == 5)
+                count++;
+        }
+    }
+
+    public static void lrCheck() {
+        int zeroCount = 0;
+        for(int i = 0; i < 5; i++) {
+            if(bingo[i][i] == 0)
+                zeroCount++;
+        }
+        if(zeroCount == 5)
+            count++;
+    }
+
+    public static void rlCheck() {
+        int zeroCount = 0;
+        for(int i = 0; i < 5; i++) {
+            if(bingo[i][4-i] == 0)
+                zeroCount++;
+        }
+        if(zeroCount == 5)
+            count++;
+    }
+}
