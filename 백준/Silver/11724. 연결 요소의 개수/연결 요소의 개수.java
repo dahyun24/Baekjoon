3ebee8fa7435150d051c269dd51e4def4ecd3b10
다현 <123882512+dahyun24@users.noreply.github.com>
2025-08-19 01:10:39 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 640 ms, Memory: 142148 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int count;
+
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+
+    // DFS (재귀)
+    public static void dfs(int v) {
+        visited[v] = true;
+        for (int next : graph.get(v)) {
+            if (!visited[next]) dfs(next);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // 그래프 초기화
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        visited = new boolean[N+1];
+        count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
